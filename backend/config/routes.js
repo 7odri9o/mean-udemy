63a68e9fe9a36f58fa99099549e2d28bf14243b8
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-11-23 22:43:59 -0300

    50. Registrando as Rotas do Serviço de Ciclo de Pagamentos

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -4,4 +4,7 @@ module.exports = function(server) {
 
     const router = express.Router()
     server.use('/api', router)
+
+    const billingCycleService = require('../api/billing-cycle/billing-cycle-service')
+    billingCycleService.register(router, '/billingCycles')
 }
