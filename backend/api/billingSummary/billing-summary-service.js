812fdc31e1dc055fd769d5a12a4c479a6276f1ac
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-11-24 01:19:06 -0300

    56. Registrando a Rota do Serviço de Sumário de Pagamentos

--- backend/api/billingSummary/billing-summary-service.js
+++ backend/api/billingSummary/billing-summary-service.js
@@ -2,13 +2,13 @@ const _ = require('lodash')
 const billingCycle = require('../billing-cycle/billing-cycle')
 
 function getSummary(req, res) {
-    billingCycle.aggregate({
+    billingCycle.aggregate([{
         $project: { credit: { $sum: "$credits.value" }, debts: { $sum: "$debts.value" } }
     }, {
-        $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" }}
+        $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debts" }}
     }, {
         $project: { _id: 0, credit: 1, debt: 1 }
-    }, function (error, result) {
+    }], function (error, result) {
         if(error) {
             res.status(500).json({errors: [error]})
         } else {
@@ -17,4 +17,4 @@ function getSummary(req, res) {
     })
 }
 
-module.exports { getSummary }
+module.exports = { getSummary }
