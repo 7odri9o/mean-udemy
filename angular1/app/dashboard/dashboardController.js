65f48059ad35e2b9e1b38f9c62552146acc356a3
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-11-25 23:44:46 +0000

    77. Atualizando o DashboardController para Angular 1.6

--- angular1/app/dashboard/dashboardController.js
+++ angular1/app/dashboard/dashboardController.js
@@ -7,7 +7,8 @@ angular.module('meanControleFinanceiro').controller('DashboardCtrl', [
 function DashboardController(scope, http) {
     $scope.getSummary = function() {
         const url = 'http://192.168.56.102:3003/api/billingSummary'
-        $http.get(url).success(function({credit = 0, debt = 0}) {
+        $http.get(url).then(function(response) {
+            const { credit = 0, debt = 0 } = response.data
             $scope.credit = credit
             $scope.debt = debt
             $scope.total = credit - debt
