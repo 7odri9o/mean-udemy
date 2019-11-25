e3c0642cec4d846554be94109e310237e91dbba5
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-11-25 23:41:42 +0000

    76. Dashboard Controller

---
+++ angular1/app/dashboard/dashboardController.js
@@ -0,0 +1,18 @@
+angular.module('meanControleFinanceiro').controller('DashboardCtrl', [
+    '$scope',
+    '$http',
+    DashboardController
+])
+
+function DashboardController(scope, http) {
+    $scope.getSummary = function() {
+        const url = 'http://192.168.56.102:3003/api/billingSummary'
+        $http.get(url).success(function({credit = 0, debt = 0}) {
+            $scope.credit = credit
+            $scope.debt = debt
+            $scope.total = credit - debt
+        })
+    }
+
+    $scope.getSummary()
+}
