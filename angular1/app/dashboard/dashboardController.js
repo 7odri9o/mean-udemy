5b2f17d74b3ab8552e56e5e2f5ca1a11e3eaa936
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-11-26 23:16:05 +0000

    78. Habilitando o CORS na Aplicação Backend - correção dos erros cols

--- angular1/app/dashboard/dashboardController.js
+++ angular1/app/dashboard/dashboardController.js
@@ -4,16 +4,17 @@ angular.module('meanControleFinanceiro').controller('DashboardCtrl', [
     DashboardController
 ])
 
-function DashboardController(scope, http) {
+function DashboardController($scope, $http) {
     $scope.getSummary = function() {
         const url = 'http://192.168.56.102:3003/api/billingSummary'
         $http.get(url).then(function(response) {
+            console.log(response)
             const { credit = 0, debt = 0 } = response.data
             $scope.credit = credit
             $scope.debt = debt
             $scope.total = credit - debt
         })
     }
-
+    
     $scope.getSummary()
 }
