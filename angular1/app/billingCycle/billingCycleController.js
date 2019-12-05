537bd45b3904eb2722d2a72a6155731d9d51f56c
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-12-05 18:00:14 +0000

    85. Factory de Exibição de Mensagens

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,17 +1,18 @@
 (function() {
     angular.module('meanControleFinanceiro').controller('BillingCycleCtrl', [
         '$http',
+        'msgs',
         BillingCycleController
     ])
 
-    function BillingCycleController($http) {
+    function BillingCycleController($http, msgs) {
         const vm = this
         vm.create = function () {
             const url = 'http://192.168.56.102:3003/api/billingCycles'
             $http.post(url, vm.billingCycle).then(function(response) {
                 vm.billingCycle = {}
-                console.log('Successo')
-            })
+                msgs.addSuccess('Operação realizada com sucesso!')
+            }).catch(response => msgs.addError(response.data.errors))
         }
     }
 })()
