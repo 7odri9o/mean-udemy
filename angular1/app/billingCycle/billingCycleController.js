fbbba1f62ab6d3845c860acf00eff6284130abeb
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-12-08 22:31:06 +0000

    88. Factory para controlar a exibição das abas

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,28 +1,29 @@
-(function() {
-    angular.module('meanControleFinanceiro').controller('BillingCycleCtrl', [
-        '$http',
-        'msgs',
-        BillingCycleController
-    ])
+angular.module('meanControleFinanceiro').controller('BillingCycleCtrl', [
+    '$http',
+    'msgs',
+    'tabs',
+    BillingCycleController
+])
 
-    function BillingCycleController($http, msgs) {
-        const vm = this
-        const url = 'http://192.168.56.102:3003/api/billingCycles'
+function BillingCycleController($http, msgs, tabs) {
+    const vm = this
+    const url = 'http://192.168.56.102:3003/api/billingCycles'
+    console.log('Criou BillingCycleController')
 
-        vm.refresh = function() {
-            $http.get(url).then(function(response) {
-                vm.billingCycle = {}
-                vm.billingCycles = response.data
-            }).catch(response => msgs.addError(response.data.errors))
-        }
-
-        vm.create = function () {
-            $http.post(url, vm.billingCycle).then(function(response) {
-                vm.refresh()
-                msgs.addSuccess('Operação realizada com sucesso!')
-            }).catch(response => msgs.addError(response.data.errors))
-        }
-
-        vm.refresh()
+    vm.refresh = function() {
+        $http.get(url).then(response => {
+            vm.billingCycle = {}
+            vm.billingCycles = response.data
+            tabs.show(vm, { tabList: true, tabCreate: true })
+        }).catch(response => msgs.addError(response.data.errors))
     }
-})()
+
+    vm.create = function () {
+        $http.post(url, vm.billingCycle).then(function(response) {
+            vm.refresh()
+            msgs.addSuccess('Operação realizada com sucesso!')
+        }).catch(response => msgs.addError(response.data.errors))
+    }
+
+    vm.refresh()
+}
